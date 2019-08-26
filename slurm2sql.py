a72773b1201224f2615afbceb8a2744df6b82be4
Richard Darst <rkd@zgib.net>
2019-08-26 10:12:56 +0300

    Improve datay

--- slurm2sql.py
+++ slurm2sql.py
@@ -1,11 +1,21 @@
 import argparse
-import csv
+import json
 import os
+import re
 import sqlite3
 import subprocess
-import sys
+
+def nullint(x):
+    """int or None"""
+    return int(x) if x else None
+
+def nullstr(x):
+    """str or None"""
+    return str(x) if x else None
 
 def slurmtime(x):
+    """Parse slurm time of format [dd-[hh:]]mm:ss"""
+    if not x: return None
     seconds = 0
     if '-' in x:
         days, time = x.split('-', 1)
@@ -19,8 +29,9 @@ def slurmtime(x):
     return seconds
 
 def slurmmem(x):
+    """Memory, removing 'n' or 'c' at end, in KB"""
+    if not x:  return None
     x = x.strip('Knc')
-    if x == '': return None
     return float(x)//1024
 
 class linefunc(object):
@@ -29,22 +40,77 @@ class slurmMemNode(linefunc):
     @staticmethod
     def calc(row):
         reqmem = row['ReqMem']
+        if not reqmem:  return None
         if reqmem.endswith('c'):
-            return slurmmem(reqmem) * int(row['NCPUS']) / int(row('NNodes'))
+            return slurmmem(reqmem) * int(row['NCPUS']) / int(row['NNodes'])
         if reqmem.endswith('n'):
             return slurmmem(reqmem)
 class slurmMemType(linefunc):
     @staticmethod
     def calc(row):
         reqmem = row['ReqMem']
-        if reqmem.endswith('n'): return 'n'
-        if reqmem.endswith('c'): return 'c'
+        if not reqmem: return None
+        if reqmem.endswith('n'):  return 'n'
+        if reqmem.endswith('c'):  return 'c'
         raise ValueError("Unknown memory type")
 class slurmMemRaw(linefunc):
     @staticmethod
     def calc(row):
         return row['ReqMem']
+class slurmReqGPU(linefunc):
+    @staticmethod
+    def calc(row):
+        gres = row['ReqGRES']
+        if not gres:  return None
+        m = re.search('gpu:(\d+)', gres)
+        if m:
+            return int(m.group(1))
+class slurmGPUMem(linefunc):
+    @staticmethod
+    def calc(row):
+        comment = row['Comment']
+        if not comment.strip():  return
+        if 'No GPU stats' in comment:  return
+        comment = json.loads(comment)
+        return comment['gpu_mem_max']
+class slurmGPUUtil(linefunc):
+    @staticmethod
+    def calc(row):
+        comment = row['Comment']
+        if not comment.strip(): return
+        if 'No GPU stats' in comment:  return
+        comment = json.loads(comment)
+        return comment['gpu_util']/100.
+class slurmGPUCount(linefunc):
+    @staticmethod
+    def calc(row):
+        comment = row['Comment']
+        if not comment.strip():  return
+        if 'No GPU stats' in comment:  return
+        comment = json.loads(comment)
+        return comment['ngpu']
 
+#class slurmMemEff(linefunc):
+#    #https://github.com/SchedMD/slurm/blob/master/contribs/seff/seff
+#    @staticmethod
+#    def calc(row):
+#        walltime = slurmtime(row['Elapsed'])
+#        reqmemtype = slurmMemType(row)
+#        reqmem = int(row['ReqMem'].strip('Knc'))
+#        if reqmemtype == 'c':
+#            reqmem = reqmem * int(row['NCPUS'])
+#            pernode = False
+#        else:
+#            reqmem = reqmem * int(row['NNodes'])
+#            pernode = True
+class slurmCPUEff(linefunc):
+    #https://github.com/SchedMD/slurm/blob/master/contribs/seff/seff
+    @staticmethod
+    def calc(row):
+        elapsed = slurmtime(row['Elapsed'])
+        if not elapsed: return None
+        cpueff = slurmtime(row['TotalCPU']) / (elapsed * int(row['NCPUS']))
+        return cpueff
 
 COLUMNS = {
     'JobID': str,
@@ -54,30 +120,40 @@ COLUMNS = {
     'Start': str,
     'End': str,
 
-    'TimeLimit': slurmtime,
+    'Timelimit': slurmtime,
+    'State': str,
+    'Elapsed': slurmtime,
+    'Partition': str,
     #'ReqTRES': str,
-    #'ReqGRES':
+    'ReqGRES': str,
     #'Comment'
-    #'NTasks'
+    'NTasks': nullint,
+    '_ReqGPU': slurmReqGPU,
 
     #'AllocGRES'
     #'AllocTRES'
 
-    'NCPUS': int,
-    'NNodes': int,
+    'NNodes': nullint,
+    'AllocNodes': nullint,
 
-    'AllocCPUS': int,
-    'CPUTime': slurmtime,   # = CPUTimeRaw
-    'TotalCPU': slurmtime,
+    'NCPUS': nullint,
+    'CPUTime': slurmtime,   # = Elapsed * NCPU    (= CPUTimeRaw)  (not how much used)
+    'TotalCPU': slurmtime,  # = Elapsed * NCPU * efficiency
+    'UserCPU': slurmtime,
+    'SystemCPU': slurmtime,
+    'AllocCPUS': nullint,
+    '_CPUEff': slurmCPUEff,
 
     'ReqMem': slurmMemNode,
     '_ReqMemType': slurmMemType,
     '_ReqMemRaw': slurmMemRaw,
     'MaxRSS': slurmmem,
     'AveRSS': slurmmem,
-    'State': str,
-    'Elapsed': slurmtime,
-    'Partition': str,
+
+    'Comment': nullstr,
+    '_GPUMem': slurmGPUMem,
+    '_GPUUtil': slurmGPUUtil,
+    '_NGPU': slurmGPUCount,
     }
 
 if __name__ == "__main__":
@@ -93,14 +169,27 @@ if __name__ == "__main__":
     create_columns = ', '.join(c.strip('_') for c in COLUMNS)
     create_columns = create_columns.replace('JobIDRaw', 'JobIDRaw UNIQUE')
     db.execute('CREATE TABLE IF NOT EXISTS slurm (%s)'%create_columns)
+    db.execute('CREATE TABLE IF NOT EXISTS slurm (%s)'%create_columns)
     c = db.cursor()
 
-    cols = ','.join(c for c in COLUMNS.keys() if not c.startswith('_'))
-    p = subprocess.Popen(['sacct', '-o', cols, '-P', '--units=K',
-                          *args.sacct_filter],
+    slurm_cols = tuple(c for c in COLUMNS.keys() if not c.startswith('_'))
+    cmd = ['sacct', '-o', ','.join(slurm_cols), '-P', '--units=K',
+           #'--allocations',  # no job steps, only total jobs, but doesn't show used resources.
+           *args.sacct_filter]
+    print(' '.join(cmd))
+    p = subprocess.Popen(cmd,
                          stdout=subprocess.PIPE, universal_newlines=True)
 
-    for line in csv.DictReader(p.stdout, delimiter='|'):
+    # We don't use the csv module because the csv can be malformed.
+    # In particular, job name can include newlines.  TODO: handle job
+    # names with newlines.
+    for i, line in enumerate(p.stdout):
+        if i == 0:  continue
+        line = line.split('|')
+        if len(line) < len(slurm_cols):
+            continue
+        line = dict(zip(slurm_cols, line))
+
         #print(line)
         processed_line = {k.strip('_'): (COLUMNS[k](line[k])
                                          #if not isinstance(COLUMNS[k], type) or not issubclass(COLUMNS[k], linefunc)
